e5da0b3ac1bc1660f384e882f2d1230099ee5061
Aaronlamz <qylinxia@qq.com>
2022-03-16 21:51:33 +0800

    update config

--- deploy.sh
+++ deploy.sh
@@ -18,10 +18,5 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Aaronlamz/node-weekly.git master:gh-pages
-
-cd -
+git push -f git@github.com:Aaronlamz/node-weekly.git main:gh-pages
