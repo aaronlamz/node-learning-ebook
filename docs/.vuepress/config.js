2236d389628e5667441a6cb55f6f3fb5c8a72898
Aaronlamz <qylinxia@qq.com>
2022-03-19 16:03:52 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'NodeJS 知识体系',
-  description: 'NodeJS 知识体系整理、技术分享',
-  base:'/node-weekly/'
+  title: 'Node.js 学习指南',
+  description: 'Node.js 学习笔记/指南、构建前端知识体系',
+  base:'/node-learning/'
 }
