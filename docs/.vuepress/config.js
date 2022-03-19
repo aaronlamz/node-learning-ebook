ad4f658776296b6342ee4cdf17b592a8d215201c
Aaronlamz <qylinxia@qq.com>
2022-03-19 18:40:32 +0800

    fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: 'Node.js 学习指南',
-  description: 'Node.js 学习笔记/指南、构建前端知识体系',
+  description: 'Node.js 学习笔记指南、构建前端知识体系',
   base:'/node-learning/',
   themeConfig: {
     logo: '/nodejs.svg',
